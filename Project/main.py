bc9b552ff433abd1166a681f67f0dd76ca6c4f05
omgabay <44643796+omgabay@users.noreply.github.com>
2023-02-11 00:09:35 +0000

    trying hdbscan for clustering

--- Project/main.py
+++ Project/main.py
@@ -1,21 +1,16 @@
 import json
 import pandas as pd 
-import numpy as np 
+import numpy as np
 from sentence_transformers import SentenceTransformer
 from compare_clustering_solutions import evaluate_clustering
 from sklearn.metrics import pairwise_distances_argmin_min
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import CountVectorizer
 import nltk 
-import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
-from sklearn import metrics
-from nltk.tokenize import RegexpTokenizer
-import nltk
-from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
-
-
+import umap 
+import hdbscan
 
 def find_reps_for_cluster(clusters, num_reps):
     model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
@@ -29,11 +24,10 @@ def find_reps_for_cluster(clusters, num_reps):
         cluster["representative_sentences"].extend([data[idx] for idx in closest])   
 
 
-
 def suggest_topic(cluster):
     requests = [request for request in cluster["requests"]]
     stopwords = ['me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now']
-    possible_pos_tags = {('VBN', 'IN', 'NN'), ('VB', 'TO', 'DT', 'NN'), ('JJ', 'VBP', 'DT'), ('VB', 'IN', 'DT', 'NN'), ('VB', 'DT', 'NN', 'NN'), ('VBN', 'IN', 'DT', 'NN'), ('WP', 'PRP', 'VBP', '.'), ('VB', 'RB', 'TO', 'NN'), ('RB', 'NNS'), ('JJ', 'NNS'), ('VBN', 'NN'), ('NN', 'TO', 'DT', 'NNS'), ('VBN', 'TO', 'NN'), ('NN', 'NN'), ('VB', 'NNS'), ('IN', 'DT', 'NN'), ('NN', 'IN', 'NN'), ('NNS', 'NN'), ('VB', 'DT', 'NN'), ('NN', 'NNS'), ('VBG', 'NN')}
+    possible_pos_tags = {('VBN', 'IN', 'NN'), ('VB', 'TO', 'DT', 'NN'), ('JJ', 'VBP', 'DT'), ('VB', 'IN', 'DT', 'NN'), ('VB', 'DT', 'NN', 'NN'), ('VBN', 'IN', 'DT', 'NN'), ('WP', 'PRP', 'VBP', '.'),('WP','PRP','VBP'), ('VB', 'RB', 'TO', 'NN'), ('RB', 'NNS'), ('JJ', 'NNS'), ('VBN', 'NN'), ('NN', 'TO', 'DT', 'NNS'), ('VBN', 'TO', 'NN'), ('NN', 'NN'), ('VB', 'NNS'), ('NN', 'IN', 'NN'), ('NNS', 'NN'), ('VB', 'DT', 'NN'), ('NN', 'NNS'), ('VBG', 'NN')}
     c_vec = CountVectorizer(ngram_range=(2,4))
 
     # matrix of ngrams
@@ -44,14 +38,14 @@ def suggest_topic(cluster):
     # list of ngrams
     vocab = c_vec.vocabulary_    
 
-    # sort ngrams by their respective count 
+    # sort ngrams by their respective count
     ngram_sorted = sorted([(count_values[i],k) for k,i in vocab.items()], reverse=True)
  
     # heading is prioritized based on num of appearnces in requests and length of the topics - prefer longer topic names over shorter
     heading = None  # holds the current best heading 
     for count, ngram in ngram_sorted:
         if heading is not None and float(count)/heading[0] < 0.7:
-            print(f'existing after seeing "{ngram}" final="{heading[1]}" count ratio {float(count)/heading[0]}')
+            print(f'chosen heading "{heading[1]}" | existed after seeing "{ngram}" count ratio {float(count)/heading[0]}')
             return
 
         tokenized_ngram = nltk.word_tokenize(ngram)
@@ -59,11 +53,11 @@ def suggest_topic(cluster):
         pos_tags = [tagging[1] for tagging in nltk.pos_tag(tokenized_ngram)]
         if tuple(pos_tags) in possible_pos_tags:    
             if heading is None:     
-                print(f'possible heading "{ngram}" counted {count} tokenized:{tokenized_ngram}  pos tags: {pos_tags}')
+                print(f'possible heading "{ngram}" counted {count} times. Tokens:{tokenized_ngram} POS tags: {pos_tags}')
                 cluster["cluster_name"] = ngram        
                 heading = (count,ngram,word_count)
             elif word_count > heading[2]: 
-                print(f'heading replaced with "{ngram}" counted {count} tokenized:{tokenized_ngram}  pos tags: {pos_tags}')
+                print(f'heading replaced with "{ngram}" counted {count} times. Tokens:{tokenized_ngram} POS tags: {pos_tags}')
                 cluster["cluster_name"] = ngram        
                 heading = (count,ngram,word_count)
 
@@ -79,16 +73,26 @@ def suggest_topic(cluster):
 
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
+
 def analyze_unrecognized_requests(data_file, output_file, num_rep, min_size):
     df = pd.read_csv(data_file)
     data = df["request"].to_numpy()
+  
+   
     embeddings = model.encode(data)
+    embeddings = umap.UMAP(n_neighbors=15, 
+                            n_components=5, 
+                            metric='cosine').fit_transform(embeddings)
 
-    print('shape of embeddings:',embeddings.shape)
+   
+    print('embeddings:',embeddings.shape)
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=int(min_size),
+                         metric='euclidean',                      
+                         cluster_selection_method='eom').fit(embeddings)
 
     #algorithm used: 
-    db = DBSCAN(eps=5, min_samples=int(min_size)).fit(embeddings)
-    labels = db.labels_
+    #clusterer = DBSCAN(eps=6, min_samples=int(min_size)).fit(embeddings)
+    labels = clusterer.labels_
 
     # Number of clusters in labels, ignoring noise if present.
     n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
@@ -113,14 +117,12 @@ def analyze_unrecognized_requests(data_file, output_file, num_rep, min_size):
     # create list of unclustered 
     unclustered = [data[i] for i,label in enumerate(labels) if label == -1]
 
-    # create output JSON object and write to a file 
+    # create JSON output and write to file 
     outputObj = {"cluster_list" : clusters, "unclustered" : unclustered}
     json_object = json.dumps(outputObj, indent=4)
     with open(output_file, "w+") as outfile:
-        outfile.write(json_object)
-    
-   
-
+        outfile.write(json_object)  
+  
 
 if __name__ == '__main__':
     with open('./Project/config.json', 'r') as json_file:
@@ -132,9 +134,10 @@ if __name__ == '__main__':
         solution = json.load(output)
         for cluster in solution["cluster_list"]:
             clust_name = cluster["cluster_name"]
-            pos_tags = [tagging[1] for tagging in nltk.pos_tag(nltk.word_tokenize(clust_name))]
-            tags.add(tuple(pos_tags))
-            print(pos_tags,clust_name)  
+            pos_tags = tuple([tagging[1] for tagging in nltk.pos_tag(nltk.word_tokenize(clust_name))])
+            if pos_tags not in tags: 
+                print(pos_tags,clust_name)  
+            tags.add(pos_tags)
                 
     print(f'number of different pos-tagging found in solution {len(tags)}')          
 
